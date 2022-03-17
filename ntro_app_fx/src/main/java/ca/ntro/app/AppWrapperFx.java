3265c0e5a06f08090e060d0a9ae224cbaa8a623b
Mathieu Bergeron <mathieu.bergeron@cmontmorency.qc.ca>
2022-03-17 06:24:09 -0400

    version 0.8

--- ntro_app_fx/src/main/java/ca/ntro/app/AppWrapperFx.java
+++ ntro_app_fx/src/main/java/ca/ntro/app/AppWrapperFx.java
@@ -17,95 +17,95 @@ import javafx.application.Platform;
 import javafx.stage.Stage;
 
 public class AppWrapperFx extends Application {
-	
-	static Class<? extends NtroClientFx> appClass;
-	
-	private static BackendRegistrarNtro backendRegistrar;
-	private static FrontendRegistrarFx  frontendRegistrar;
-	private static SystemTasksNtro      systemTasksNtro;
-	
-	private void onExit() {
+    
+    static Class<? extends NtroClientFx> appClass;
+    
+    private static BackendRegistrarNtro backendRegistrar;
+    private static FrontendRegistrarFx  frontendRegistrar;
+    private static SystemTasksNtro      systemTasksNtro;
+    
+    private void onExit() {
 
-		System.out.println("\n\n\n[GENERATING GRAPHS]\n\n\n");
-		NtroApp.models().writeGraphs();
+        System.out.println("\n\n\n[GENERATING GRAPHS]\n\n\n");
+        NtroApp.models().writeGraphs();
 
-		backendRegistrar.writeGraph();
-		frontendRegistrar.writeGraph();
-		systemTasksNtro.writeGraph();
+        backendRegistrar.writeGraph();
+        frontendRegistrar.writeGraph();
+        systemTasksNtro.writeGraph();
 
-	}
+    }
 
-	@Override
-	public void start(Stage primaryStage) throws Exception {
-		
-		NtroApp.registerLocaleService(new LocaleServiceJdk());
-		NtroApp.registerExitService(new ExitServiceJdk());
-		
-		NtroClientFx app = Ntro.factory().newInstance(appClass);
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        
+        NtroApp.registerLocaleService(new LocaleServiceJdk());
+        NtroApp.registerExitService(new ExitServiceJdk());
+        
+        NtroClientFx app = Ntro.factory().newInstance(appClass);
 
-		MessageRegistrarNtro messageRegistrar  = new MessageRegistrarNtro();
-		ModelRegistrarNtro   modelRegistrar    = new ModelRegistrarNtro();
-		backendRegistrar  = new BackendRegistrarNtro();
-		frontendRegistrar = new FrontendRegistrarFx();
-		systemTasksNtro   = new SystemTasksNtro();
+        MessageRegistrarNtro messageRegistrar  = new MessageRegistrarNtro();
+        ModelRegistrarNtro   modelRegistrar    = new ModelRegistrarNtro();
+        backendRegistrar  = new BackendRegistrarNtro();
+        frontendRegistrar = new FrontendRegistrarFx();
+        systemTasksNtro   = new SystemTasksNtro();
 
-		WindowFx window = new WindowFx(primaryStage);
-		frontendRegistrar.setWindow(window);
+        WindowFx window = new WindowFx(primaryStage);
+        frontendRegistrar.setWindow(window);
 
-		primaryStage.setOnCloseRequest(event -> {
-			Platform.runLater(() -> {
-				NtroApp.exit(() -> onExit());
-			});
-		});
-		
-		backendRegistrar.setMessageRegistrar(messageRegistrar);
-		backendRegistrar.setModelRegistrar(modelRegistrar);
+        primaryStage.setOnCloseRequest(event -> {
+            Platform.runLater(() -> {
+                NtroApp.exit(() -> onExit());
+            });
+        });
+        
+        backendRegistrar.setMessageRegistrar(messageRegistrar);
+        backendRegistrar.setModelRegistrar(modelRegistrar);
 
-		frontendRegistrar.setMessageRegistrar(messageRegistrar);
-		frontendRegistrar.setModelRegistrar(modelRegistrar);
+        frontendRegistrar.setMessageRegistrar(messageRegistrar);
+        frontendRegistrar.setModelRegistrar(modelRegistrar);
 
-		app.registerMessages(messageRegistrar);
-		app.registerModels(modelRegistrar);
-		app.registerBackend(backendRegistrar);
-		app.registerFrontend(frontendRegistrar);
+        app.registerMessages(messageRegistrar);
+        app.registerModels(modelRegistrar);
+        app.registerBackend(backendRegistrar);
+        app.registerFrontend(frontendRegistrar);
 
-		System.out.println("\n\n\n");
-		System.out.println("[INFO] Ntro version 0.5");
-		System.out.println("[INFO] Locale: '" + NtroApp.currentLocale() + "'");
+        System.out.println("\n\n\n");
+        System.out.println("[INFO] Ntro version 0.8");
+        System.out.println("[INFO] Locale: '" + NtroApp.currentLocale() + "'");
 
-		// XXX: must first prepare every task graph
-		//      (creates trace)
-		backendRegistrar.prepareToExecuteTasks();
-		frontendRegistrar.prepareToExecuteTasks();
-		systemTasksNtro.prepareToExecuteTasks();
-		
-		modelRegistrar.watchModels();
+        // XXX: must first prepare every task graph
+        //      (creates trace)
+        backendRegistrar.prepareToExecuteTasks();
+        frontendRegistrar.prepareToExecuteTasks();
+        systemTasksNtro.prepareToExecuteTasks();
+        
+        modelRegistrar.watchModels();
 
-		backendRegistrar.executeTasks();
-		frontendRegistrar.executeTasks();
-		systemTasksNtro.executeTasks();
-		
-		// FIXME: should define some Ntro.threadService
-		new Thread() {
-			
-			@Override
-			public void run() {
+        backendRegistrar.executeTasks();
+        frontendRegistrar.executeTasks();
+        systemTasksNtro.executeTasks();
+        
+        // FIXME: should define some Ntro.threadService
+        new Thread() {
+            
+            @Override
+            public void run() {
 
-				System.out.println("[INFO] App running. Press Enter here to exit.");
+                System.out.println("[INFO] App running. Press Enter here to exit.");
 
-				try {
+                try {
 
-					System.in.read();
-					Platform.exit();
-					NtroApp.exit(() -> onExit());
+                    System.in.read();
+                    Platform.exit();
+                    NtroApp.exit(() -> onExit());
 
-				} catch (IOException e) {
+                } catch (IOException e) {
 
-					e.printStackTrace();
-				}
-			}
-		}.start();
+                    e.printStackTrace();
+                }
+            }
+        }.start();
 
-	}
+    }
 
 }
