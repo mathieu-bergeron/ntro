c3133c418356f18cb50fe16314a5d60f9e713338
Mathieu Bergeron <mathieu.bergeron@cmontmorency.qc.ca>
2022-03-25 13:06:16 -0400

    plusieurs balles

--- ntro_app_fx/src/main/java/ca/ntro/app/world2d/Object2dFx.java
+++ ntro_app_fx/src/main/java/ca/ntro/app/world2d/Object2dFx.java
@@ -3,9 +3,12 @@ package ca.ntro.app.world2d;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.input.MouseEvent;
 
-public abstract class Object2dFx extends Object2d<GraphicsContext, 
-                                                  Object2dFx,
-                                                  World2dFx> {
+public abstract class Object2dFx<OBJECT2D extends Object2dFx<OBJECT2D, WORLD2D>,
+                                 WORLD2D extends World2dFx<OBJECT2D, WORLD2D>> 
+       
+      extends Object2d<GraphicsContext, 
+                       OBJECT2D,
+                       WORLD2D> {
 	
 	protected abstract boolean onMouseEvent(MouseEvent evtFx, double x, double y);
 
--- ntro_app_fx/src/main/java/ca/ntro/app/world2d/World2dFx.java
+++ ntro_app_fx/src/main/java/ca/ntro/app/world2d/World2dFx.java
@@ -3,13 +3,16 @@ package ca.ntro.app.world2d;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.input.MouseEvent;
 
-public abstract class World2dFx extends World2d<GraphicsContext, 
-                                                Object2dFx,
-                                                World2dFx> {
+public abstract class World2dFx<OBJECT2D extends Object2dFx<OBJECT2D, WORLD2D>,
+                                WORLD2D  extends World2dFx<OBJECT2D, WORLD2D>> 
+
+       extends World2d<GraphicsContext, 
+                       OBJECT2D,
+                       WORLD2D> {
 
 	public void dispatchMouseEvent(MouseEvent evtFx, double x, double y) {
 		boolean consumed = false;
-		for(Object2dFx object : getObjects()) {
+		for(OBJECT2D object : getObjects()) {
 			if(object.collidesWith(x-2, y-2, 4, 4)) {
 				consumed = consumed || object.onMouseEvent(evtFx, x, y);
 			}
