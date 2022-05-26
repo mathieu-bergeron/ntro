fe97ac05187e78ba3206daf1307acb0cb9c16c97
Mathieu Bergeron <mathieu.bergeron@cmontmorency.qc.ca>
2022-05-26 10:28:54 -0400

    support pour Array en Json

--- ntro_core_jdk/src/main/java/ca/ntro/core/reflection/ObjectBuilderJdk.java
+++ ntro_core_jdk/src/main/java/ca/ntro/core/reflection/ObjectBuilderJdk.java
@@ -1,7 +1,11 @@
 package ca.ntro.core.reflection;
 
+import java.lang.reflect.Array;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.lang.reflect.TypeVariable;
+import java.util.List;
+
 
 import ca.ntro.core.initialization.Ntro;
 import ca.ntro.core.reflection.object_graph.ObjectBuilderNtro;
@@ -29,6 +33,8 @@ public class ObjectBuilderJdk extends ObjectBuilderNtro {
 			if(method == null) {
 				Ntro.throwException("[FATAL] setter not found: " + setterName(object, setterName));
 			}
+			
+			attributeValue = convertListToArrayIfNecessary(method, attributeValue);
 
 			method.invoke(object, attributeValue);
 
@@ -42,6 +48,55 @@ public class ObjectBuilderJdk extends ObjectBuilderNtro {
 		}
 	}
 	
+	private static Object convertListToArrayIfNecessary(Method method, Object attributeValue) {
+		Object result = attributeValue;
+		
+		Class<?> paramType = null;
+
+		if(method.getParameterTypes().length > 0) {
+			
+			paramType = method.getParameterTypes()[0];
+
+		}else {
+			
+			Ntro.throwException("[FATAL] setter must have a least one input parameter " + method.getName());
+
+		}
+
+		if(paramType != null
+				&& paramType.isArray()
+				&& attributeValue != null
+				&& attributeValue instanceof List) {
+			
+			result = convertListToArray(paramType, (List<?>) attributeValue);
+
+		}
+		
+		return result;
+	}
+
+	private static Object convertListToArray(Class<?> arrayType, List<?> list) {
+
+		Class<?>  componentType = arrayType.getComponentType();
+		
+		Object array = Array.newInstance(componentType, list.size());
+		
+		for(int i = 0; i < list.size(); i++) {
+			Object value = list.get(i);
+
+			if(componentType.isArray()
+					&& value != null
+					&& value instanceof List) {
+				
+				value = convertListToArray(componentType, (List<?>) value);
+			}
+
+			Array.set(array, i, value);
+		}
+
+		return array;
+	}
+
 	private static String setterName(Object object, String setterName) {
 		return Ntro.reflection().simpleName(object.getClass()) + "." + setterName;
 	}
